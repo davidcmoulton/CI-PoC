13c74822c2626b5d9c171522894a814d8b03b9ed
David Moulton <d.moulton@elifesciences.org>
2013-07-02 20:21:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ CI-PoC
 ======
 
 Various tests and experiments with continuous integration.
+
+Nothing to see here, really.
---
+++ spec/triangle.spec.js
@@ -0,0 +1,59 @@
+// The code for triangle. Move to own file once this is running
+var SHAPES = SHAPES || {};
+SHAPES.triangle = function (sides) {
+  var _tri = {};
+  if (!sides.length || sides.length !== 3) {
+    throw new TypeError("sides argument is a amandatory 3-elelemt array.")
+  }
+  _tri.sides = sides;
+
+  return _tri;
+};
+
+// Tests describing a suite of functions operating on triangles
+
+// test the construction of a triangle
+describe("Building a triangle", function () {
+
+  it("exists", function () {
+    var triangle = SHAPES.triangle([3, 4, 5]);
+    expect(triangle).toBeDefined();
+  });
+
+  it("has the sides you create it with", function () {
+    var triangle = SHAPES.triangle([3, 4, 5]),
+        sides = triangle.sides;
+    expect(sides.length).toEqual(3);
+    expect(sides).toContain(3);
+    expect(sides).toContain(4);
+    expect(sides).toContain(5);
+
+  });
+});
+
+
+// test a triangle's properties and methods
+describe("A triangle", function () {
+  var triangle;
+
+  beforeEach(function () {
+    triangle = SHAPES.triangle([3, 4, 5]);
+  });
+  
+  afterEach(function () {
+    triangle = null;
+  });
+
+
+  it("has three sides", function () {
+    var sides = triangle.sides;
+    expect(sides.length).toEqual(3);
+  });
+
+});
+
+
+
+(function () {
+  (jasmine.getEnv()).execute();
+}());
